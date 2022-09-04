6c79bceabf949871bac0f3e3962cb11a013ab633
eusouobn <86791806+eusouobn@users.noreply.github.com>
2022-09-04 11:50:23 -0300

    Update user-test.sh

--- user-test.sh
+++ user-test.sh
@@ -4,4 +4,4 @@ printf '\x1bc';
 
 read -p "Digite o Nome de Usuário : " USERNAME
 
-arch-chroot /mnt "su - $USERNAME systemctl --user enable pipewire.service"
+arch-chroot /mnt "su - $USERNAME" "systemctl --user enable pipewire.service"
